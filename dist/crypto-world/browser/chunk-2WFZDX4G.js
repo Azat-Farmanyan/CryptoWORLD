2adb6074972ddfae0515b861938d4f088e8173b6
Azat Farmanyan <azat.farmanyan2@mail.ru>
2024-02-03 13:00:19 +0400

    feat: buid app

---
+++ dist/crypto-world/browser/chunk-2WFZDX4G.js
@@ -0,0 +1,1 @@
+var c="https://api.coingecko.com/api/v3/search/trending",o="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd";export{c as a,o as b};
---
+++ dist/crypto-world/browser/chunk-DXHRM6NG.js
@@ -0,0 +1,1 @@
+import{c as r,i as e}from"./chunk-LRLJ6IC5.js";var s=(()=>{let t=class t{constructor(){this.searchedWord=new r("")}};t.\u0275fac=function(i){return new(i||t)},t.\u0275prov=e({token:t,factory:t.\u0275fac,providedIn:"root"});let o=t;return o})();export{s as a};
